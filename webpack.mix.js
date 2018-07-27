621954e51539d53140411a565a0aea946d607bbe
SebastiaN <odm_sd@hotmail.com>
2018-07-27 17:57:34 -0500

    Correccion de tema

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .scripts(
+        [
+            './resources/assets/tema/global/plugins/respond.min.js',
+            './resources/assets/tema/global/plugins/excanvas.min.js',
+            './resources/assets/tema/global/plugins/ie8.fix.min.js',
+            './resources/assets/tema/global/plugins/jquery.min.js',
+            './resources/assets/tema/global/plugins/bootstrap/js/bootstrap.min.js',
+            './resources/assets/tema/global/plugins/js.cookie.min.js',
+            './resources/assets/tema/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js',
+            './node_modules/moment/moment.js',
+            './node_modules/moment/locale/es.js',
+            './resources/assets/tema/global/plugins/jquery.blockui.min.js',
+            './resources/assets/tema/global/plugins/bootstrap-select/js/bootstrap-select.min.js',
+            './resources/assets/tema/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
+            './node_modules/bootstrap-daterangepicker/daterangepicker.js',
+            './resources/assets/tema/global/scripts/app.min.js',
+            './resources/assets/tema/layouts/layout5/scripts/layout.min.js',
+            './resources/assets/tema/layouts/global/scripts/quick-sidebar.min.js',
+        ],
+        './public/js/vendor.js'
+    ).sourceMaps()
+    .copyDirectory('resources/assets/logos', 'public/images')
+    .js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/turnero.js', 'public/js');
