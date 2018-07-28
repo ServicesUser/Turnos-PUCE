ef2396a6ce563c8d752052da461770ce77414c3a
SebastiaN <odm_sd@hotmail.com>
2018-07-28 16:25:02 -0500

    login completo

--- resources/views/componentes/invitado/login_panel.blade.php
+++ resources/views/componentes/invitado/login_panel.blade.php
@@ -1,4 +1,4 @@
-<div class="m-grid__item m-grid__item--fluid m-grid m-grid--center m-grid--hor m-grid__item--order-tablet-and-mobile-1	m-login__content" >
+<div class="m-grid__item m-grid__item--fluid m-grid m-grid--center m-grid--hor m-grid__item--order-tablet-and-mobile-1	m-login__content fondo" >
     <div class="m-grid__item m-grid__item--middle">
         <div class="m-login__welcome">
             <object type="image/svg+xml" data="{{asset('images/logo_puce.svg')}}" class="img-responsive"></object>
