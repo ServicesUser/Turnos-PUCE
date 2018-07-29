bfea87ccfd86117701e4cd045fd1f52871cf6515
SebastiaN <odm_sd@hotmail.com>
2018-07-28 19:19:57 -0500

    rutas con autenticacion

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function (){
+    Route::get('/inicio', 'HomeController@index')->name('home');
+});
