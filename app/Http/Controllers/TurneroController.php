b7b545594e2036b5e8f5c4e7f9b0748dba437142
SebastiaN <odm_sd@hotmail.com>
2018-07-31 15:34:28 -0500

    query mejorado paraque muestre los ultimos atendidos

--- app/Http/Controllers/TurneroController.php
+++ app/Http/Controllers/TurneroController.php
@@ -14,8 +14,9 @@ class TurneroController extends Controller
 
     public function tiemporeal(){
         $fecha  =   Date::now()->format('Y-m-d H:i:s');
+        $fecha2  =   Date::now()->format('Y-m-d H:')."00:00";
         $toca   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos natural join estado_turno where DATE_FORMAT(CONCAT(fecha_tu,' ',inicio_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et=2 and fecha_tu=DATE('$fecha') order by inicio_tu limit 12");
-        $paso   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos natural join estado_turno where DATE_FORMAT(CONCAT(fecha_tu,' ',fin_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et>2 and fecha_tu=DATE('$fecha') order by inicio_tu limit 12");
+        $paso   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos natural join estado_turno where DATE_FORMAT(CONCAT(fecha_tu,' ',fin_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha2','%Y-%m-%d %H:%i:%s') and id_et>2 and fecha_tu=DATE('$fecha') order by inicio_tu limit 12");
         return (['toca'=>$toca,'paso'=>$paso]);
     }
 }
