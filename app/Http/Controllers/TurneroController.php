9a3a11f8dc49b0525602bb4d7c6baac6a57f6974
SebastiaN <odm_sd@hotmail.com>
2018-07-31 14:32:12 -0500

    correccion de query

--- app/Http/Controllers/TurneroController.php
+++ app/Http/Controllers/TurneroController.php
@@ -14,8 +14,8 @@ class TurneroController extends Controller
 
     public function tiemporeal(){
         $fecha  =   Date::now()->format('Y-m-d H:i:s');
-        $toca   =   DB::select("SELECT turnos.id_tu,nu_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos where DATE_FORMAT(CONCAT(fecha_tu,' ',inicio_tu), '%Y-%m-%d %H:%i:%s')=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et=1 order by inicio_tu");
-        $paso   =   DB::select("SELECT turnos.id_tu,nu_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos where DATE_FORMAT(CONCAT(fecha_tu,' ',fin_tu), '%Y-%m-%d %H:%i:%s')=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et!=1 order by inicio_tu limit 6");
+        $toca   =   DB::select("SELECT turnos.id_tu,nu_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos where DATE_FORMAT(CONCAT(fecha_tu,' ',inicio_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha','Y-%m-%d %H:%i:%s') and id_et=2 order by inicio_tu limit 15");
+        $paso   =   DB::select("SELECT turnos.id_tu,nu_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos where DATE_FORMAT(CONCAT(fecha_tu,' ',fin_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et>2 order by inicio_tu limit 6");
         return (['toca'=>$toca,'paso'=>$paso]);
     }
 }
