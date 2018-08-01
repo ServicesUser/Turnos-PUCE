92a66ea29afa1b9e9d83613ed6f2545c1465d6c2
SebastiaN <odm_sd@hotmail.com>
2018-08-01 10:34:44 -0500

    logo no tan grande

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="{{asset('images/logo_puce.png')}}" alt="{{ $slot }}" style="max-width:20%; border:none">
+            <img src="{{asset('images/logo_puce.png')}}" alt="{{ $slot }}" style="max-width:12%; border:none">
         </a>
     </td>
 </tr>
