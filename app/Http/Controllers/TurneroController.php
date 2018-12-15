2594b77c164a821012ea74657ffc8e9317070f70
SebastiaN <odm_sd@hotmail.com>
2018-12-15 14:54:06 -0500

    consulta con parametros

--- app/Http/Controllers/TurneroController.php
+++ app/Http/Controllers/TurneroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Jenssegers\Date\Date;
 
 class TurneroController extends Controller
@@ -11,6 +12,11 @@ class TurneroController extends Controller
     public function vista(){
         return view('turnero');
     }
+
+    public function vista2(){
+        return view('turnero2');
+    }
+
 
     public function tiemporeal(){
         $fecha  =   Date::now()->format('Y-m-d H:i:s');
@@ -20,10 +26,29 @@ class TurneroController extends Controller
         return (['toca'=>$toca,'paso'=>$paso]);
     }
 
+    public function tiempoPuesto(Request $datos){
+        $validator = Validator::make($datos->all(), [
+            'fecha' =>  'required|date',
+            'desde' =>  'required|numeric',
+            'hasta' =>  'required|numeric|gt:desde',
+        ]);
+        if ($validator->fails()) {
+            $linea = '';
+            foreach ($validator->errors()->all() as $item) {
+                $linea .= "$item</br>";
+            }
+            return (['val'=>false,'mensaje'=>$linea,'errores'=>$validator->errors()]);
+        }else{
+            $fecha  =   Date::createFromFormat('Y-m-d H:i:s',$datos->fecha." ".$datos->desde.":00:00");
+            $fecha2 =   Date::createFromFormat('Y-m-d H:i:s',$datos->fecha." ".$datos->hasta.":00:00");
+            $toca   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos natural join estado_turno where DATE_FORMAT(CONCAT(fecha_tu,' ',inicio_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha','%Y-%m-%d %H:%i:%s') and id_et=2 and fecha_tu=DATE('$fecha') order by inicio_tu,detalle_cu ASC ");
+            $paso   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join estudiantes natural join  cubiculos natural join estado_turno where DATE_FORMAT(CONCAT(fecha_tu,' ',fin_tu), '%Y-%m-%d %H:%i:%s')>=DATE_FORMAT('$fecha2','%Y-%m-%d %H:%i:%s') and id_et>2 and fecha_tu=DATE('$fecha') order by inicio_tu,detalle_cu ASC");
+            return (['val'=>true,'toca'=>$toca,'paso'=>$paso,'desde'=>$fecha,'hasta'=>$fecha2]);
+        }
+    }
+
     public function disponibles(){
         $toca   =   DB::select("SELECT turnos.id_tu,nu_tu,turnos.id_et,inicio_tu,fin_tu,detalle_cu,fecha_tu,nombre_et FROM turnos natural join horarios natural join users natural join  cubiculos natural join estado_turno where turnos.id_et=1 order by fecha_tu,inicio_tu ");
-
         return view('disponibles',['disponibles'=>$toca]);
-
     }
 }
