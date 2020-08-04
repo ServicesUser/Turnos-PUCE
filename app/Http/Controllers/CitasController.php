a815352bd408c9298dfe32d7f006ef1e45f65b55
Sebastian <sebastian.robalino@inpsercom.com>
2020-08-03 23:09:57 -0500

    formato fecha

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -23,8 +23,8 @@ class CitasController extends Controller
     public function cola(){
         $usuario    =   Auth::user()->id;
         $fecha      =   Date::now()->format('Y-m-d');
-        $pendientes =   DB::select("SELECT turnos.id_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es FROM turnos natural join horarios natural join dias natural join users natural join estudiantes where fecha_di=$fecha and id_et=2 and users.id=$usuario order by inicio_tu");
-        $completos  =   DB::select("SELECT turnos.id_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es FROM turnos natural join horarios natural join dias natural join users natural join estudiantes where fecha_di=$fecha and id_et=3 and users.id=$usuario order by inicio_tu");
+        $pendientes =   DB::select("SELECT turnos.id_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es FROM turnos natural join horarios natural join dias natural join users natural join estudiantes where fecha_di=DATE_FORMAT('$fecha','%Y-%m-%d') and id_et=2 and users.id=$usuario order by inicio_tu");
+        $completos  =   DB::select("SELECT turnos.id_tu,inicio_tu,fin_tu,estudiantes.cedula_es,nombres_es,apellidos_es,email_es FROM turnos natural join horarios natural join dias natural join users natural join estudiantes where fecha_di=DATE_FORMAT('$fecha','%Y-%m-%d') and id_et=3 and users.id=$usuario order by inicio_tu");
 
         return ([
             'pendeientes'   =>  count($pendientes),
