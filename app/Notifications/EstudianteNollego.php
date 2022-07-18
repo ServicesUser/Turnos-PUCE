6cedcd67309f8fe2945a79f8655b8627ba56e60c
Sebastian <sebastian.robalino@inpsercom.com>
2022-07-18 09:50:14 -0500

    new: creacion de notificacion por email

---
+++ app/Notifications/EstudianteNollego.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Jenssegers\Date\Date;
+
+class EstudianteNollego extends Notification
+{
+    use Queueable;
+
+    public $datos;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($datos)
+    {
+        $this->datos=$datos;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->level('error')
+                    ->greeting('Hola '.$notifiable->nombres_es)
+                    ->line('No haz llegado a tu cita agendada')
+                    ->line('Fecha: '.Date::createFromFormat('Y-m-d', $this->datos->fecha_tu)->format('l, d F Y'))
+                    ->line('Hora: '.$this->datos->inicio_tu)
+                    ->line($this->datos->horario['responsable']['cubiculo']['detalle_cu'])
+                    ->action('Para reagendar una cita',route('turno'))
+                    ->salutation('Buen día ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
